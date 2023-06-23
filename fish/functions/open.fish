a4291d43b10446aa1507a1f92c717cf3fa13e645
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2023-06-23 11:16:53 +0530

    fish: add open.fish back

---
+++ fish/functions/open.fish
@@ -0,0 +1,20 @@
+function open
+    if not set -q argv[1]
+        printf (_ "%ls: Expected at least %d args, got only %d\n") open 1 0 >&2
+        return 1
+    end
+
+    if [ -d "$argv" ]
+        cd "$argv"
+    else if file "$argv" | grep -q 'ASCII text'
+        $EDITOR "$argv"
+    else if [ -f "$argv" ]
+        termux-share "$argv" &
+    else if echo "$argv" | rg '^https?://.*\.(jpg|png|gif|jpeg)' &>/dev/null
+        curl -o - "$argv" | timg -w 5 -
+    else if type -qf xdg-open
+        xdg-open "$argv" &
+    else
+        echo (_ 'No open utility found. Try installing "xdg-open" or "xdg-utils".') >&2
+    end
+end
