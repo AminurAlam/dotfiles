5e972ba38f082c67ad75e67558c5aa17fdefa1aa
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2024-03-19 18:47:39 +0530

    scripts: lsp installer

---
+++ scripts/setup_lsp.fish
@@ -0,0 +1,30 @@
+[ -n "$XDG_PROJECTS_DIR" ] || set XDG_PROJECTS_DIR "$HOME/repos"
+set patches "$XDG_PROJECTS_DIR/dotfiles/scripts/patches"
+
+# npm i -g bash-language-server
+pacman -Syu --noconfirm --needed clang rust-analyzer lua-language-server
+
+function python
+    cd "$XDG_PROJECTS_DIR"
+    pacman -Syu --noconfirm --needed ruff python-pip
+
+    git clone -q --depth 1 "https://github.com/astral-sh/ruff-lsp"
+    patch ruff-lsp/pyproject.toml <"$patches/ruff-lsp-pyproject.toml.diff"
+
+    pip install ./ruff-lsp pyright python-lsp-server
+
+    yes | rm -rf ruff-lsp
+end
+
+function java
+    cd "$XDG_PROJECTS_DIR"
+    pacman -Syu --noconfirm --needed openjdk-17 maven
+
+    git clone -q --depth 1 "https://github.com/georgewfraser/java-language-server"
+    patch java-language-server/pom.xml <"$patches/java-language-server-pom.xml.diff"
+
+    ./java-language-server/scripts/link_linux.sh
+    mvn package -DskipTests -Dmaven.javadoc.skip
+
+    # yes | rm -rf java-language-server
+end
