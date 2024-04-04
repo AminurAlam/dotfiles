109b8a9b8db656c3c066c0faaa7832df351dc206
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2024-04-04 09:26:33 +0530

    setup: support only one architecture

--- setup.fish
+++ setup.fish
@@ -1,12 +1,18 @@
-set arch (uname -m | sed 's/^arm.*/arm/')
+#!/usr/bin/env fish
+
 [ -n "$XDG_PROJECTS_DIR" ] || set XDG_PROJECTS_DIR "$HOME/repos"
-set packages clang dust eza libgit2 fd git openssh python python-pip renameutils ripgrep starship lua-language-server termux-api zoxide
+
+# packages
+set base_packages clang dust eza libgit2 fd git openssh python python-pip renameutils ripgrep starship lua-language-server termux-api zoxide
+set python_packages requests "git+https://github.com/nathom/streamrip.git@dev" yt-dlp "$XDG_PROJECTS_DIR/deflacue" # TODO: "$XDG_PROJECTS_DIR/musicbrainzpy"
+
 # urls
 set url_font "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/SauceCodeProNerdFont-Medium.ttf"
 set url_dotfiles "https://github.com/AminurAlam/dotfiles.git"
 set url_neovim "https://github.com/AminurAlam/neovim.git"
 set url_fish_setup "https://raw.githubusercontent.com/AminurAlam/dotfiles/main/setup.fish"
 set url_bootstrap "https://github.com/termux-pacman/termux-packages/releases"
+
 # paths
 set main "/sdcard/main/termux/"
 set path_dots "$XDG_PROJECTS_DIR/dotfiles" # NOTE: make sure this is a full path
@@ -15,18 +21,12 @@ set path_nvim "$XDG_PROJECTS_DIR/nvim-fork"
 command mkdir -p $XDG_PROJECTS_DIR $HOME/{backup,repos}/ $HOME/.local/{share,bin,cache}/ $HOME/.local/share/zoxide/
 
 printf "INSTALLING NEW PACKAGES...\n"
-pacman -Syu --noconfirm --needed $packages || begin
-    printf "failed, try installing manually:
-    $(set_color $fish_color_command)pacman $(set_color $fish_color_option)-Syu --needed $(set_color $fish_color_param)$packages $(set_color normal)\n"
-    exit
-end
+    pacman -Syu --noconfirm --needed $base_packages || exit
 
-if command -vq python && command -vq pip
-    printf "INSTALLING PYTHON PACKAGES\n"
+printf "INSTALLING PYTHON PACKAGES...\n"
     git clone -q --depth 1 "https://github.com/AminurAlam/deflacue" "$XDG_PROJECTS_DIR/deflacue"
     git clone -q --depth 1 "https://github.com/AminurAlam/musicbrainzpy" "$XDG_PROJECTS_DIR/musicbrainzpy"
-    pip install "$XDG_PROJECTS_DIR/deflacue" "$XDG_PROJECTS_DIR/musicbrainzpy" requests
-end
+    pip install $python_packages
 
 printf "DOWNLOADING DOTFILES... "
     [ -d "$path_dots" ] && command rm -fr "$path_dots"
@@ -36,7 +36,7 @@ printf "DOWNLOADING DOTFILES... "
 printf "done\n"
 
 printf "LINKING CONFIG DIRECTORIES... "
-for config in aria2 clangd fish flacon git htop newsboat npm nvim python streamrip yt-dlp
+for config in aria2 clangd fish git htop newsboat npm nvim python streamrip yt-dlp zellij
     [ -e "$path_dots/$config" ] || continue
     # unlink/relocate old directories in ~/.config
     [ -L "$HOME/.config/$config" ] && command unlink "$HOME/.config/$config"
@@ -49,9 +49,9 @@ printf "LINKING CONFIG FILES... "
     ln -fs "$path_dots/other/clang-format" ~/.clang-format
     ln -fs "$path_dots/other/curlrc" ~/.config/.curlrc
     ln -fs "$path_dots/other/pacman.conf" $PREFIX/etc/pacman.conf
+    ln -fs "$path_dots/other/tidal-dl.json" ~/.config/.tidal-dl.json
     ln -fs "$path_dots/other/starship.toml" ~/.config/starship.toml
     ln -fs "$path_dots/other/stylua.toml" ~/.config/stylua.toml
-    ln -fs "$path_dots/other/tidal-dl.json" ~/.config/.tidal-dl.json
     ln -fs "$path_dots/termux/colors.properties" ~/.termux/colors.properties
     ln -fs "$path_dots/termux/termux.properties" ~/.termux/termux.properties
 printf "done\n"
@@ -71,8 +71,8 @@ printf "CLEANUP... "
     [ -d ~/storage/ ] && for path in ~/storage/*
         [ -L "$path" ] && unlink "$path"
     end
-    command rmdir --ignore-fail-on-non-empty --parents ~/backup/**/ ~/storage/
-    command rm ~/bootstrap-*.zip
+    command rmdir --ignore-fail-on-non-empty --parents ~/backup/** ~/storage
+    command rm (fd -d1 'bootstrap-aarch64.zip' $HOME)
 printf "done\n"
 
 # printf "ADDING ZOXIDE DB... "
--- setup.sh
+++ setup.sh
@@ -1,24 +1,19 @@
 #!/usr/bin/env bash
 
 termux_setup="https://raw.githubusercontent.com/AminurAlam/dotfiles/main/setup.fish"
-root_url="https://github.com/termux-pacman/termux-packages/releases"
+boot_url="https://github.com/termux-pacman/termux-packages/releases/latest/download/bootstrap-aarch64.zip"
+boot_path="$HOME/bootstrap-aarch64.zip"
 
 bootstrap-pacman() {
     printf "BOOTSTRAPPING PACMAN...\n"
-    printf "determining arch... "
-    arch="$(uname -m | sed 's/^arm.*/arm/')"
-    printf "%s\n" "$arch"
 
-    bootstrap_path="$HOME/bootstrap-${arch}.zip"
-
-    printf "downloading bootstrap...\n"
-    [ -e "$bootstrap_path" ] || curl --create-dirs -q#Lo "$bootstrap_path" -- "${root_url}/latest/download/bootstrap-${arch}.zip"
+    [ -e "$boot_path" ] || curl --create-dirs -q#Lo "$boot_path" -- "$boot_url"
 
     mkdir -p ~/../usr-n/
     cd ~/../usr-n/ || exit
 
     printf "extracting bootstrap... "
-    unzip -qd ~/../usr-n/ "$bootstrap_path"
+    unzip -qd ~/../usr-n/ "$boot_path"
     printf "done\n"
 
     printf "creating symlinks... "
