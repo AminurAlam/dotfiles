c36893ac7fe8bd936bf81f9c5c1b8e1dbea1ae12
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2024-09-07 11:44:31 +0530

    fish: update music functions

--- fish/functions/flac_to.fish
+++ fish/functions/flac_to.fish
@@ -1,18 +1,25 @@
 function flac_to -a ext
+    set -l cover # defined later
     set -l count 0
     set -l total (count *.flac) || return
     [ -z "$ext" ] && set ext opus
 
+    if fd -iq '(cover|folder)\.(jpg|png)'
+        set cover --picture (fd -i '(cover|folder)\.(jpg|png)')[1] --discard-pictures
+        echo "using `$cover[2]` as cover art"
+    end
+
     for i in *.flac
         set -l filename (string replace ".flac" "" "$i")
         set -l count (math $count + 1)
-        while [ (count (jobs)) -ge 5 ]
+        while [ (count (jobs | rg 'opusenc|ffmpeg|sox')) -ge 4 ]
             sleep 0.1
         end
-        printf " [%d/%d] %s\n" "$count" "$total" "$filename"
+        printf " [%02d/%d] %s\n" "$count" "$total" "$filename"
 
-        if [ $ext = opus ] && command -vq opusenc
-            opusenc --quiet --music --comp 10 --bitrate 256 "$filename.flac" "$filename.opus" &
+        # https://datatracker.ietf.org/doc/html/rfc7845#section-9
+        if [ "$ext" = opus ] && command -vq opusenc
+            opusenc --quiet --music --comp 10 --bitrate 256 $cover "$filename.flac" "$filename.opus" &
         else if command -vq ffmpeg
             ffmpeg -y -hide_banner -loglevel quiet -i "$filename.flac" -b:a 320k -r:a 44100 "$filename.$ext" &>/dev/null </dev/null &
         else if command -vq sox
--- fish/functions/music.fish
+++ fish/functions/music.fish
@@ -1,13 +1,12 @@
 function music
-    set files (fd -d1 '.*\.(mp3|ogg|opus|flac|lrc)' | sort -g)
-    set max (printf "%s\n" $files | wc -L)
-    set filecount (count $files)
+    set files (fd -d1 '.*\.(mp3|ogg|opus|flac|lrc)' | sort -g) # list of editable files
+    set filecount (count $files) # number of editable files
+    set max (printf "%s\n" $files | wc -L) # find length of longest string
 
     if [ "$filecount" = 0 ]
         echo no audio files
         return
-    else if [ "$filecount" = 1 ] && count *.cue
-        # split single discs
+    else if [ "$filecount" = 1 ] && count *.cue # split single discs
         set -f original (fd -d1 '.*\.(mp3|ogg|opus|flac|lrc)')
         echo $original
         deflacue -d . .
@@ -15,19 +14,19 @@ function music
         return 0
     end
 
-
-
     for file in $files
         set new (echo $file |
-            sd '^(\d+)( -|\.) ' '$1 ' |
-            sd '^(\d) ' '0$1 ')
+            sed -E 's/^([0-9]+)( -|\.) /\1 /' |
+            sed -E 's/^([0-9]) /0\1 /'
+        )
         set -f -a new_fn $new
         printf "%-"$max"s > %s\n" "$file" "$new"
     end
 
     if [ "$(read -P 'rename? [y/N] ')" = y ]
         for n in (seq $filecount)
-            [ "$files[$n]" = "$new_fn[$n]" ] || mv "$files[$n]" "$new_fn[$n]"
+            [ "$files[$n]" = "$new_fn[$n]" ]
+            or mv "$files[$n]" "$new_fn[$n]"
         end
     end
 end
