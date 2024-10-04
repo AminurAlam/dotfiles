69b1af53754462ed8147da291524c01d92c71e06
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2024-10-04 11:12:18 +0530

    yazi: still too much stuff to configure

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,37 +1,12 @@
 ---@diagnostic disable: undefined-global
+
 Status = {
   LEFT = 0,
   RIGHT = 1,
   _id = 'status',
-  _inc = 1000,
-  _left = {
-    { 'a', id = 1, order = 1000 },
-    { 'b', id = 2, order = 2000 },
-    { 'c', id = 3, order = 3000 },
-  },
-  _right = {
-    { 'x', id = 4, order = 1000 },
-    { 'y', id = 5, order = 2000 },
-    { 'z', id = 6, order = 3000 },
-  },
-  children_add = function(self, fn, order, side)
-    self._inc = self._inc + 1
-    local children = side == self.RIGHT and self._right or self._left
-
-    children[#children + 1] = { fn, id = self._inc, order = order }
-    table.sort(children, function(a, b) return a.order < b.order end)
-
-    return self._inc
-  end,
-  children_remove = function(self, id, side)
-    local children = side == self.RIGHT and self._right or self._left
-    for i, child in ipairs(children) do
-      if child.id == id then
-        table.remove(children, i)
-        break
-      end
-    end
-  end,
+  _inc = 1,
+  _left = { { 'left', id = 1, order = 1 } },
+  _right = { { 'perms', id = 2, order = 2 }, { 'right', id = 3, order = 3 } },
   children_render = function(self, side)
     local lines = {}
     for _, c in ipairs(side == self.RIGHT and self._right or self._left) do
@@ -46,13 +21,7 @@ Status = {
     }, { __index = self })
   end,
   style = function(self)
-    if self._tab.mode.is_select then
-      return THEME.status.mode_select
-    elseif self._tab.mode.is_unset then
-      return THEME.status.mode_unset
-    else
-      return THEME.status.mode_normal
-    end
+    return THEME.status[self._tab.mode.is_visual and 'mode_select' or 'mode_normal']
   end,
   render = function(self)
     local left = self:children_render(self.LEFT)
@@ -63,116 +32,69 @@ Status = {
       table.unpack(Progress:render(self._area, right:width())),
     }
   end,
-  a = function(self)
-    local mode = tostring(self._tab.mode):upper():gsub('SELECT', 'VISUAL')
-    -- local mode = tostring(self._tab.mode):sub(1, 3):upper()
-
-    local style = self:style()
-    return ui.Line {
-      -- ui.Span(THEME.status.separator_open):fg(style.bg),
-      ui.Span(' ' .. mode .. ' '):style(style),
-      ui.Span(THEME.status.separator_close):fg(style.bg):bg(THEME.status.separator_style.fg),
-    }
-  end,
-  b = function(self)
-    local h = self._tab.current.hovered
-    if not h then return ui.Line {} end
-
-    local size = ya.readable_size(h:size() or h.cha.length)
-    if h.cha.is_dir then
-      size = '' -- tostring(self._tab:name() or 'nil')
-    end
-
-    local style = self:style()
-    return ui.Line {
-      ui.Span(' ' .. size .. ' '):fg(style.bg):bg(THEME.status.separator_style.bg),
-      ui.Span(THEME.status.separator_close):fg(THEME.status.separator_style.fg),
-    }
-  end,
-  c = function(self)
-    local h = self._tab.current.hovered
-    if not h then return ui.Line {} end
-
-    local link = ''
-    local link_hl = '#7dcfff'
-
-    if h.link_to ~= nil then
-      link = ' -> ' .. tostring(h.link_to):gsub(os.getenv('HOME') or '', '~')
-      if h.cha.is_orphan then link_hl = '#ff5555' end
-    end
-
-    return ui.Line {
-      ui.Span(' ' .. h.name),
-      ui.Span(link):fg(link_hl),
-    }
-  end,
-  x = function(self)
-    local h = self._tab.current.hovered
-    if not h then return ui.Line {} end
-
-    local spans = {}
-
-    local perm = h.cha:permissions()
-    if not perm then return ui.Line {} end
-    if perm:sub(2, 3):find('-') then
-      spans = { ui.Span('󰌾'):fg('#ff5555') }
-    else
-      perm = perm:sub(1, 4)
-      for i = 1, #perm do
-        local c = perm:sub(i, i)
-        local style = THEME.status.permissions_t
-        if c == '-' or c == '?' then
-          style = THEME.status.permissions_s
-        elseif c == 'r' then
-          style = THEME.status.permissions_r
-        elseif c == 'w' then
-          style = THEME.status.permissions_w
-        elseif c == 'x' or c == 's' or c == 'S' or c == 't' or c == 'T' then
-          style = THEME.status.permissions_x
-        end
-        spans[i] = ui.Span(c):style(style)
-      end
-    end
-
-    -- if self._tab.mode.is_select then spans[#spans + 1] = ui.Span(#self._tab.selected) end
-
-    return ui.Line(spans)
-  end,
-  y = function(self)
-    local percent = 0 ---@type number|string
-    local cursor = self._tab.current.cursor
-    local length = #self._tab.current.files
-    if cursor ~= 0 and length ~= 0 then percent = math.floor((cursor + 1) * 100 / length) end
-
-    if percent == 0 then
-      percent = ' Top '
-    elseif percent == 100 then
-      percent = ' Bot '
-    else
-      percent = string.format(' %2d%% ', percent)
-    end
-
-    local style = self:style()
-    return ui.Line {
-      ui.Span(' ' .. THEME.status.separator_open):fg(THEME.status.separator_style.fg),
-      ui.Span(percent):fg(style.bg):bg(THEME.status.separator_style.bg),
-    }
-  end,
-  z = function(self)
-    -- local h = self._tab.current.hovered
-    -- return ui.Line {
-    --   ui.Span(h:name()),
-    --   ui.Span('name'),
-    -- }
-
-    local cursor = self._tab.current.cursor
-    local length = #self._tab.current.files
-
-    local style = self:style()
-    return ui.Line {
-      ui.Span(THEME.status.separator_open):fg(style.bg):bg(THEME.status.separator_style.fg),
-      ui.Span(string.format(' %2d/%-2d ', cursor + 1, length)):style(style),
-    }
-    -- ui.Span(THEME.status.separator_close):fg(style.bg),
-  end,
 }
+
+Status.left = function(self)
+  local style = self:style()
+  return ui.Line {
+    ui.Span(' ' .. tostring(self._tab.mode):upper() .. ' '):style(style),
+    ui.Span(
+      THEME.status.separator_close
+        .. ' '
+        .. ya.readable_path(tostring(cx.active.current.cwd)):gsub('(%.?)([^/])[^/]+/', '%1%2/')
+        .. ' '
+    )
+      :style(style)
+      :reverse(),
+    ui.Span(THEME.status.separator_close):fg(style.fg),
+    -- ui.Span('ddddddddddddddddddddddddddddddddddddddddddddddddddddddd'),
+  }
+end
+
+Status.perms = function(self)
+  local h = self._tab.current.hovered
+  if not h then return ui.Line {} end
+  local spans = {}
+  local perm = h.cha:permissions()
+  local perm_fg = {
+    ['d'] = '#8aadf4',
+    ['x'] = '#a6da95',
+    ['s'] = '#a6da95',
+    ['S'] = '#a6da95',
+    ['t'] = '#a6da95',
+    ['T'] = '#a6da95',
+    ['r'] = '#eed49f',
+    ['w'] = '#ed8796',
+    ['-'] = '#8087a2',
+    ['?'] = '#8087a2',
+  }
+  if perm:sub(2, 3):find('-') then
+    spans = { ui.Span('󰌾'):fg('#ff5555') }
+  else
+    for c in perm:gmatch('.') do
+      spans[#spans + 1] = ui.Span(c):fg(perm_fg[c] or '#8aadf4')
+    end
+  end
+  return ui.Line(spans)
+end
+
+Status.right = function(self)
+  local percent = 0
+  local cursor = self._tab.current.cursor
+  local length = #self._tab.current.files
+  if cursor ~= 0 and length ~= 0 then percent = math.floor((cursor + 1) * 100 / length) end
+  local pos = ''
+  if percent == 0 then
+    pos = ' Top '
+  elseif percent == 100 then
+    pos = ' Bot '
+  else
+    pos = string.format(' %2d%% ', percent)
+  end
+  local style = self:style()
+  return ui.Line {
+    ui.Span(' ' .. THEME.status.separator_open):fg(style.fg),
+    ui.Span(pos .. THEME.status.separator_open):style(style):reverse(),
+    ui.Span(string.format(' %2d/%-2d ', cursor + 1, length)):style(style),
+  }
+end
