cb168c969e997a16c6e78c4371c0a2ad70c0ecc8
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2024-12-04 09:39:49 +0530

    fish: `out` now supports rust

--- fish/functions/out.fish
+++ fish/functions/out.fish
@@ -1,7 +1,16 @@
 function out -d "compile and run c code in an instant"
     clear
 
-    cc -lm -oout -- $argv[1]
-    and ./out
-    rm out
+    set ext (string split . $argv[1])[-1]
+
+    if [ "$ext" = c ]
+        cc -lm -oout -- $argv[1]
+    else if [ "$ext" = rs ]
+        rustc -oout -- $argv[1]
+    end
+
+    [ -e out ] && begin
+        ./out
+        rm out
+    end
 end
