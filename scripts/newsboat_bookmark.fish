1744d0a1f6a9352f94b5e3a582a5634e34815693
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2025-02-22 19:45:25 +0530

    scripts: delete unused scripts

--- scripts/newsboat_bookmark.fish
+++
@@ -1,9 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/fish
-
-[ -e /sdcard/main/notes/bookmarks ] && echo "$argv[1]" >>/sdcard/main/notes/bookmarks || echo "$argv[1]" >>~/bookmarks
-
-# url="$1"
-# title="$2"
-# description="$3"
-# feed_title="$4"
-# echo -e "${url}\t${title}\t${description}\t${feed_title}" >> ~/bookmarks.txt
