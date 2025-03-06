23d3a38f051f5458e6691c7b761d9a1530987f11
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2025-03-06 20:36:22 +0530

    fish: autofill yazi arguments

--- fish/functions/y.fish
+++ fish/functions/y.fish
@@ -1,6 +1,12 @@
 function y -d "yazi wrapper to change directories"
     set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    if [ -n "$argv[1]" ] && ! [ -e "$argv[1]" ]
+        set argv[1] (zoxide query "$argv[1]")
+        [ -e "$argv[1]" ] && zoxide add "$argv[1]"
+    end
+
     yazi $argv --cwd-file="$tmp"
+
     if set cwd (command cat -- "$tmp")
         and [ -n "$cwd" ]
         and [ "$cwd" != "$PWD" ]
