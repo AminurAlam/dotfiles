f959d09802b941b7c80b1f43ac53abc808adeafb
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2025-03-10 22:33:14 +0530

    scripts: add game launcher

--- setup/linking.fish
+++ setup/linking.fish
@@ -1,4 +1,4 @@
-mkdir -p ~/backup ~/.ssh
+mkdir -p ~/backup ~/.ssh ~/.local/bin ~/bin
 
 printf "LINKING CONFIG DIRECTORIES... "
 for config in alacritty aria2 btop clangd dunst eza fish gdb git htop hypr keepassxc lazygit mpv newsboat npm nvim python ruff streamrip termux tmux waybar wofi yazi yt-dlp zathura zellij
@@ -12,26 +12,34 @@ for config in alacritty aria2 btop clangd dunst eza fish gdb git htop hypr keepa
 end
 printf "done\n"
 
-printf "LINKING CONFIG FILES... "
-    ln -fs ~/repos/dotfiles/other/clang-format  ~/.clang-format
-    ln -fs ~/repos/dotfiles/other/curlrc        ~/.config/.curlrc
-    ln -fs ~/repos/dotfiles/other/tidal-dl.json ~/.config/.tidal-dl.json
-    ln -fs ~/repos/dotfiles/other/starship.toml ~/.config/starship.toml
-    ln -fs ~/repos/dotfiles/other/stylua.toml   ~/.config/stylua.toml
-    ln -fs ~/repos/dotfiles/other/taplo.toml    ~/.config/taplo.toml
-    ln -fs ~/repos/dotfiles/other/ssh-config    ~/.ssh/config
-    if [ -e /etc/pacman.conf ]
-        sudo ln -fs ~/repos/dotfiles/other/pacman.arch.conf /etc/pacman.conf
-    end
-    if [ (uname -o) = Android ]
-        mkdir -p ~/bin
+printf "LINKING CONFIG FILES...\n"
+ln -fs ~/repos/dotfiles/other/clang-format ~/.clang-format
+ln -fs ~/repos/dotfiles/other/curlrc ~/.config/.curlrc
+ln -fs ~/repos/dotfiles/other/tidal-dl.json ~/.config/.tidal-dl.json
+ln -fs ~/repos/dotfiles/other/starship.toml ~/.config/starship.toml
+ln -fs ~/repos/dotfiles/other/stylua.toml ~/.config/stylua.toml
+ln -fs ~/repos/dotfiles/other/taplo.toml ~/.config/taplo.toml
+ln -fs ~/repos/dotfiles/other/ssh-config ~/.ssh/config
 
-        ln -fs ~/repos/dotfiles/scripts/termux-url-opener ~/bin/
-        ln -fs ~/repos/dotfiles/scripts/termux-file-editor ~/bin/
-        ln -fs ~/repos/dotfiles/other/pacman.termux.conf $PREFIX/etc/pacman.conf
-        ln -fs ~/repos/dotfiles/termux/colors.properties ~/.termux/colors.properties # https://github.com/termux/termux-app/blob/master/termux-shared/src/main/java/com/termux/shared/termux/TermuxConstants.java#L657
-        ln -fs ~/repos/dotfiles/termux/termux.properties ~/.termux/termux.properties
-    end
-printf "done\n"
+if [ -e /etc/pacman.conf ]
+    sudo ln -fs ~/repos/dotfiles/other/pacman.arch.conf /etc/pacman.conf
+end
 
-rmdir --ignore-fail-on-non-empty ~/backup
+if [ (uname -o) = Linux ]
+    ln -s ~/repos/dotfiles/scripts/bin/game ~/.local/bin/game
+end
+
+if [ (uname -o) = Android ]
+    # configs
+    ln -fs ~/repos/dotfiles/other/pacman.termux.conf $PREFIX/etc/pacman.conf
+    ln -fs ~/repos/dotfiles/termux/colors.properties ~/.termux/colors.properties # https://github.com/termux/termux-app/blob/master/termux-shared/src/main/java/com/termux/shared/termux/TermuxConstants.java#L657
+    ln -fs ~/repos/dotfiles/termux/termux.properties ~/.termux/termux.properties
+
+    # scripts
+    ln -fs ~/repos/dotfiles/scripts/bin/termux-url-opener ~/bin/termux-url-opener
+    ln -fs ~/repos/dotfiles/scripts/bin/termux-file-editor ~/bin/termux-file-editor
+    ln -fs ~/repos/dotfiles/scripts/bin/rish ~/.local/bin/rish
+    ln -fs ~/repos/dotfiles/scripts/bin/tachi ~/.local/bin/tachi
+end
+
+rmdir --ignore-fail-on-non-empty ~/backup ~/bin
