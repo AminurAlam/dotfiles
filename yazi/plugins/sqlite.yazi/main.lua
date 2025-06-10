99edbb27deb7d4d1b01f2a48332cab942609e17a
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2025-06-10 23:00:24 +0530

    yazi: fix some colors

--- yazi/plugins/sqlite.yazi/main.lua
+++ yazi/plugins/sqlite.yazi/main.lua
@@ -1,46 +1,20 @@
 local M = {}
 
 function M:peek(job)
-  local args = {
-    tostring(job.file.url),
-    '.schema',
-  }
-
-  local child = Command('sqlite3'):args(args):stdout(Command.PIPED):stderr(Command.PIPED):spawn()
-  local limit = job.area.h
-  local i, lines = 0, ''
-  repeat
-    local next, event = child:read_line()
-    if event == 1 then
-      return require('code').peek(job)
-    elseif event ~= 0 then
-      break
-    end
-
-    i = i + 1
-    if i > job.skip then lines = lines .. next end
-  until i >= job.skip + limit
-
-  child:start_kill()
-  if job.skip > 0 and i < job.skip + limit then
-    ya.manager_emit('peek', {
-      tostring(math.max(0, i - limit)),
-      only_if = job.file.url,
-      upper_bound = true,
+  local child, err = Command('sqlite3')
+    :arg({
+      tostring(job.file.url),
+      '.schema',
     })
-  else
-    lines = lines:gsub('\t', string.rep(' ', PREVIEW.tab_size))
-    ya.preview_widgets(job, { ui.Text.parse(lines):area(job.area) })
-  end
-end
+    :stdout(Command.PIPED)
+    :output()
 
-function M:seek(job)
-  local h = cx.active.current.hovered
-  if not h or h.url ~= job.file.url then return end
-  ya.manager_emit('peek', {
-    math.max(0, cx.active.preview.skip + job.units),
-    only_if = job.file.url,
-  })
+  if not err then
+    text = child.stdout
+    ya.preview_widget(job, ui.Text.parse(text):area(job.area):wrap(ui.Text.WRAP))
+  else
+    ya.preview_widget(job, ui.Text.parse(err):area(job.area):wrap(ui.Text.WRAP))
+  end
 end
 
 return M
