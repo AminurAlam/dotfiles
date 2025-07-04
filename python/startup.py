557603dece68eb7bc1d1ababc4203de45f21fa4f
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2025-07-04 12:34:48 +0530

    python: organize imports

--- python/startup.py
+++ python/startup.py
@@ -1,12 +1,8 @@
-import json
 import base64
 import io
+import json
+import math
 import os
 import random
-import sys
-import math
 import readline
-
-readline.set_auto_history(True)
-sys.ps1 = "❯ "
-sys.ps2 = "… "
+import sys
