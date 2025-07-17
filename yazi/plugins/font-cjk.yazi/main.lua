92fa0587191c87e76fd513fdd07d8f039d783b5e
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2025-07-17 12:32:16 +0530

    yazi: formatting

--- yazi/plugins/font-cjk.yazi/main.lua
+++ yazi/plugins/font-cjk.yazi/main.lua
@@ -1,56 +1,49 @@
--- TODO: cjk font
-local TEXT = "ABCD abcd\noO0 1lI\n1234567890\n!@#$%^&*()[]{}\n== <= >= !=\nこれ直楽糸"
+local TEXT = 'ABCD abcd\noO0 1lI\n1234567890\n!@#$%^&*()[]{}\n== <= >= !=\nこれ直楽糸'
 
 local M = {}
 
 function M:peek(job)
-	local start, cache = os.clock(), ya.file_cache(job)
-	if not cache then
-		return
-	end
+  local start, cache = os.clock(), ya.file_cache(job)
+  if not cache then return end
 
-	local ok, err = self:preload(job)
-	if not ok or err then
-		return
-	end
+  local ok, err = self:preload(job)
+  if not ok or err then return end
 
-	ya.sleep(math.max(0, rt.preview.image_delay / 1000 + start - os.clock()))
+  ya.sleep(math.max(0, rt.preview.image_delay / 1000 + start - os.clock()))
 
-	local _, err = ya.image_show(cache, job.area)
-	ya.preview_widget(job, err and ui.Text(err):area(job.area):wrap(ui.Wrap.YES))
+  local _, err = ya.image_show(cache, job.area)
+  ya.preview_widget(job, err and ui.Text(err):area(job.area):wrap(ui.Wrap.YES))
 end
 
 function M:seek() end
 
 function M:preload(job)
-	local cache = ya.file_cache(job)
-	if not cache or fs.cha(cache) then
-		return true
-	end
+  local cache = ya.file_cache(job)
+  if not cache or fs.cha(cache) then return true end
 
-	local status, err = Command("magick"):arg({
-		"-size",
-		"700x800",
-		"-gravity",
-		"center",
-		"-font",
-		tostring(job.file.url),
-		"-pointsize",
-		64,
-		"xc:white",
-		"-fill",
-		"black",
-		"-annotate",
-		"+0+0",
-		TEXT,
-		"JPG:" .. tostring(cache),
-	}):status()
+  local status, err = Command('magick'):arg({
+    '-size',
+    '700x800',
+    '-gravity',
+    'center',
+    '-font',
+    tostring(job.file.url),
+    '-pointsize',
+    64,
+    'xc:white',
+    '-fill',
+    'black',
+    '-annotate',
+    '+0+0',
+    TEXT,
+    'JPG:' .. tostring(cache),
+  }):status()
 
-	if status then
-		return status.success
-	else
-		return true, Err("Failed to start `magick`, error: %s", err)
-	end
+  if status then
+    return status.success
+  else
+    return true, Err('Failed to start `magick`, error: %s', err)
+  end
 end
 
 return M
