02d22f17f2c8c81dbb5441665917b711e027ef70
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2025-08-01 19:31:11 +0530

    nvim(lsp): add tinymist

---
+++ nvim/lsp/tinymist.lua
@@ -0,0 +1,37 @@
+---@type vim.lsp.Config
+return {
+  cmd = { 'tinymist' },
+  filetypes = { 'typst' },
+  root_markers = { '.git' },
+  on_attach = function(client, bufnr)
+    for _, command in ipairs {
+      -- 'tinymist.exportSvg',
+      'tinymist.exportPng',
+      'tinymist.exportPdf',
+      'tinymist.exportHtml', -- Use typst 0.13
+      'tinymist.exportMarkdown',
+      -- 'tinymist.exportText',
+      -- 'tinymist.exportQuery',
+      -- 'tinymist.getServerInfo',
+      -- 'tinymist.getDocumentTrace',
+      -- 'tinymist.getWorkspaceLabels',
+      -- 'tinymist.getDocumentMetrics',
+    } do
+      vim.api.nvim_buf_create_user_command(
+        bufnr,
+        'Typst' .. command:match 'tinymist%.export(%w+)',
+        function()
+          client:exec_cmd({
+            title = command,
+            command = command,
+            arguments = { vim.api.nvim_buf_get_name(bufnr) },
+          }, { bufnr = bufnr }, function(err, res)
+            if err then return vim.notify(err.code .. ': ' .. err.message, vim.log.levels.ERROR) end
+            -- vim.notify(res, vim.log.levels.INFO)
+          end)
+        end,
+        { nargs = 0, desc = command }
+      )
+    end
+  end,
+}
