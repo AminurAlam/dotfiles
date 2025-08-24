e8d0c2e9628542ac4367a65a30d77e969dd7b476
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2025-08-24 20:49:29 +0530

    setup(windows): use scoop instead of winget

--- setup/windows.ps1
+++ setup/windows.ps1
@@ -1,10 +1,47 @@
-winget install Neovim.Neovim Neovide.Neovide Git.Git Mozilla.Firefox astral-sh.ruff
-winget upgrade Neovim.Neovim
+if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
+    Write-Host "downloading scoop..."
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+    Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+} else {
+    Write-Host "updating scoop..."
+    scoop update
+}
 
-cd ~
-git clone --depth 1 "https://github.com/AminurAlam/dotfiles"
-Copy-item -Force -Recurse -Verbose .\dotfiles\nvim\ -Destination .\AppData\Local\
 
-python -m pip install pyright python-language-server
+Write-Host "adding scoop buckets..."
+scoop bucket add versions
+scoop bucket add extras
+scoop bucket add nerd-fonts
 
-nvim
+
+Write-Host "installing packages..."
+if (-not (Get-Command aria2  -ErrorAction SilentlyContinue)) { scoop install aria2 }
+scoop config aria2-warning-enabled false
+
+scoop install git pwsh yazi versions/neovim-nightly extras/kanata extras/alacritty nerd-fonts/SourceCodePro-NF-Mono
+# scoop install ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick
+
+# scoop update *
+# scoop checkup
+
+
+Write-Host "downloading dotfiles..."
+
+if (Test-Path -Path ~/dotfiles -PathType Container) {
+    Set-Location ~/dotfiles ;
+    git pull
+} else {
+    git clone --depth 1 "https://github.com/AminurAlam/dotfiles" ~/dotfiles ;
+}
+
+New-Item -Path ~/AppData/Local/nvim/ -ItemType SymbolicLink -Target ~/dotfiles/nvim/
+New-Item -Path ~/AppData/alacritty/ -ItemType SymbolicLink -Target ~/dotfiles/alacritty/
+New-Item -Path ~/AppData/yazi/ -ItemType SymbolicLink -Target ~/dotfiles/yazi/
+
+if ($PSVersionTable.PSEdition -eq "Core") {
+    New-Item -Path "~/Documents/Powershell/" -ItemType SymbolicLink -Target ~/dotfiles/powershell/
+} elseif ($PSVersionTable.PSEdition -eq "Desktop") {
+    New-Item -Path "~/Documents/WindowsPowerShell/" -ItemType SymbolicLink -Target ~/dotfiles/powershell/
+}
+
+# irm https://christitus.com/win | iex
