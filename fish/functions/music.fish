72a09fdaf1e2131c30bf8cab284bbe8437db2c4a
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2025-09-02 09:44:57 +0530

    fish(music): use `qmv` for rename

--- fish/functions/music.fish
+++ fish/functions/music.fish
@@ -1,5 +1,5 @@
 function music -d "process music files with a single command"
-    set files (fd -d1 '.*\.(mp3|ogg|opus|flac|lrc)' | sort -g) # list of editable files
+    set files (fd -d1 '.*\.(mp3|ogg|opus|flac|m4a|lrc)' | sort -g) # list of editable files
     set filecount (count $files) # number of editable files
     set max (printf '%s\n' $files | wc -L) # find length of longest string
 
@@ -14,19 +14,7 @@ function music -d "process music files with a single command"
         return 0
     end
 
-    for file in $files
-        set new (echo $file |
-            sed -E 's/^([0-9]+)( -|\.) /\1 /' |
-            sed -E 's/^([0-9]) /0\1 /'
-        )
-        set -f -a new_fn $new
-        printf "%-"$max"s > %s\n" "$file" "$new"
-    end
-
-    if [ "$(read -P 'rename? [y/N] ')" = y ]
-        for n in (seq $filecount)
-            [ "$files[$n]" = "$new_fn[$n]" ]
-            or mv "$files[$n]" "$new_fn[$n]"
-        end
-    end
+    command -vq qmv
+    and qmv -f do --editor 'nvim +"%s/\\v^(\\d+)( -|\\.) /\\1 /e" +"%s/\\v^(\\d) /0\\1 /e"' -- $files
+    or printf "insatll `renameutils` and try again"
 end
