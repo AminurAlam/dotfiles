ae43ae27f051cfed29f151517e3fc9988cdfcb7a
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2025-09-07 12:13:20 +0530

    fish(gcp): minor spelling mistake

--- fish/functions/gcp.fish
+++ fish/functions/gcp.fish
@@ -4,16 +4,17 @@ function gcp -a url path branch -d "git clone wrapper"
         command -vq termux-clipboard-get && set url (termux-clipboard-get)
 
         [ -z "$url" ] && echo "ERROR: no url given" && return
+        # TODO: check if its a proper url
     end
     [ -n "$branch" ] && set -f branch "--branch" "$branch"
     [ -e "$HOME/repos" ] || mkdir "$HOME/repos"
-    set is_gh string match -q -- "https://github.com/" "$url"
+    set is_gh string match -qr -- "^https://github.com/" "$url"
 
     # TODO: only for github links
     $is_gh && set url (string replace -r -- 'https://([^/]+)/([^/]+)/([^/]+).*' 'https://$1/$2/$3' "$url")
 
     if $is_gh && [ -e ~/.ssh/github_ed25519 ]
-        set url (string replace -r -- 'https://github.com/([^/]+)/([^/]+)' 'git@gittub.com:$1/$2.git' "$url")
+        set url (string replace -r -- 'https://github.com/([^/]+)/([^/]+)' 'git@github.com:$1/$2.git' "$url")
     # else if [ -e ~/.ssh/gitlab_ed25519 ] && string match -q -- "https://gitlab.com/" "$url"
     # else if [ -e ~/.ssh/codeberg_ed25519 ] && string match -q -- "https://codeberg.org/" "$url"
     # else if [ -e ~/.ssh/sourcehut_ed25519 ] && string match -q -- "https://git.sr.ht/" "$url"
@@ -21,6 +22,7 @@ function gcp -a url path branch -d "git clone wrapper"
 
     cd "$HOME/repos"
 
+    echo $url
     git clone --depth 1 $branch -- $url $path
     and set success true
 
