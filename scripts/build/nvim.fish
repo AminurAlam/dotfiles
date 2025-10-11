bfbb959083ddb341b6e74854d9159903c8e21572
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2025-10-11 21:03:16 +0530

    scripts(build): redlib

--- scripts/build/nvim.fish
+++ scripts/build/nvim.fish
@@ -1,53 +1,10 @@
-set REPO_NAME nvim-fork
-set REPO_PATH "$HOME/repos/$REPO_NAME"
-set REPO_URL "git@github.com/AminurAlam/neovim.git"
-set DEPENDENCIES binutils clang cmake gettext libunibilium libuv luajit luv \
-    make ninja openssl pkg-config tree-sitter tree-sitter-parsers utf8proc
-
-function pre_build
-    if command -vq apt
-        set -f pm apt install
-    else if command -vq pacman
-        set -f pm pacman -S --noconfirm --needed
-    else if command -vq dnf
-        set -f pm dnf install
-    else
-        printf "cannot determine package manager\n"
-        exit
-    end
-
-    $pm -- $DEPENDENCIES
-
-    [ -d "$REPO_PATH" ] || git clone --branch custom "$REPO_URL" "$REPO_PATH"
-    cd "$REPO_PATH"
-    git remote show | grep -q upstream || git remote add upstream "git@github.com:neovim/neovim.git"
-    git fetch upstream
-    git rebase upstream/master || begin
-        printf "rebase failed...\n"
-        printf "try running `git mergetool` `git rebase --continue`...\n"
-        exit
-    end
+if not [ -d ~/repos/nvim-fork/ ]
+    gcp https://github.com/AminurAlam/neovim nvim-fork
 end
 
-function build
-    if [ -e build -o -e .deps ] && [ "$(read -P 'run `make distclean`? [y/N] ')" = y ]
-        [ -e "$PREFIX/share/nvim" ] && rm -fr "$PREFIX/share/nvim/"
-        make distclean
-    end
+cd ~/repos/nvim-fork/ || exit
+git fetch upstream
+git rebase upstream/master
 
-    make BUNDLED_CMAKE_FLAG="-G Ninja -DUSE_BUNDLED=OFF" || exit
-    make install BUNDLED_CMAKE_FLAG="-G Ninja -W no-dev" CMAKE_INSTALL_PREFIX="$PREFIX/"
-end
-
-function post_build
-    [ -L "$PREFIX"/share/nvim/runtime/parser ] || ln -s "$PREFIX"/lib/tree_sitter "$PREFIX"/share/nvim/runtime/parser
-
-    build/bin/nvim -V1 --version || return 1
-end
-
-
-[ "$argv[1]" = quick ] || pre_build
-
-build
-
-post_build
+cd ~/repos/dotfiles/scripts/build/nvim/ || exit
+makepkg -si
