66ae058e7df2fcefc45916b4429fe1d355c99eae
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2026-01-17 22:51:07 +0530

    fish: fix autopair not working

--- fish/conf.d/bindings.fish
+++ fish/conf.d/bindings.fish
@@ -2,7 +2,7 @@ status is-interactive || exit
 
 fish_vi_key_bindings
 
-bind yy fish_clipboard_copy
+bind yy fish_clipboard_copy # TODO: only copy a line not entire block
 bind p fish_clipboard_paste
 
 bind q exit # shortcuts to quit
@@ -24,3 +24,32 @@ bind -M insert alt-h backward-char
 bind -M insert alt-j down-or-search
 bind -M insert alt-k up-or-search
 bind -M insert alt-l forward-char
+
+### autopair
+# https://github.com/jorgebucaran/autopair.fish
+
+set --global autopair_left \( \[ \{ \" \'
+set --global autopair_right \) \] \} \" \'
+set --global autopair_pairs "()" "[]" "{}" '""' "''"
+
+set --query fish_key_bindings[1] || return
+
+test $fish_key_bindings = fish_default_key_bindings &&
+    set --local mode default insert ||
+    set --local mode insert default
+
+bind --mode $mode[-1] --erase \177 \b \t
+
+bind --mode $mode[1] \177 _autopair_backspace # macOS ⌫
+bind --mode $mode[1] \b _autopair_backspace
+bind --mode $mode[1] \t _autopair_tab
+
+printf "%s\n" $autopair_pairs | while read --local left right --delimiter ""
+    bind --mode $mode[-1] --erase $left $right
+    if test $left = $right
+        bind --mode $mode[1] $left "_autopair_insert_same \\$left"
+    else
+        bind --mode $mode[1] $left "_autopair_insert_left \\$left \\$right"
+        bind --mode $mode[1] $right "_autopair_insert_right \\$right"
+    end
+end
