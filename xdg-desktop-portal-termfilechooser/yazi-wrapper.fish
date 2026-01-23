3aa3e0e1479a3d4e0949e05615888e61902d36ad
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2026-01-23 12:59:16 +0530

    xdg-desktop-portal-termfilechooser: fix saving files with spaces in them

--- xdg-desktop-portal-termfilechooser/yazi-wrapper.fish
+++ xdg-desktop-portal-termfilechooser/yazi-wrapper.fish
@@ -1,4 +1,4 @@
-#!/usr/bin/env fish
+#!/usr/bin/fish --no-config
 
 set multiple $argv[1]
 set directory $argv[2]
@@ -7,30 +7,28 @@ set path $argv[4]
 set out $argv[5]
 
 # https://fishshell.com/docs/current/cmds/trap.html
-function cleanup --on-signal EXIT -s HUP -s INT -s QUIT -s ABRT -s TERM
-    notify-send TODO cleanup
-    # [ -f "$tmpfile" ]
-    # and /usr/bin/rm "$tmpfile"
-    # or :
-    #
-    # if [ "$save" = 1 ] && [ ! -s "$out" ]
-    #     /usr/bin/rm "$path" || :
-    # end
-end
+# function cleanup --on-signal EXIT -s HUP -s INT -s QUIT -s ABRT -s TERM
+#     notify-send TODO cleanup
+#     [ -f "$tmpfile" ]
+#     and /usr/bin/rm "$tmpfile"
+#     or :
+#
+#     if [ "$save" = 1 ] && [ ! -s "$out" ]
+#         /usr/bin/rm "$path" || :
+#     end
+# end
 
 if [ "$save" = 1 ]
     set tmpfile (/usr/bin/mktemp)
     /usr/bin/printf '%s' '=== xdg-desktop-portal-termfilechooser file ===' >"$path"
-    set args --chooser-file=(string escape -- $tmpfile) (string escape -- $path)
+    set args --chooser-file (string escape -- $tmpfile)
 else if [ "$directory" = 1 ]
-    set args --cwd-file=(string escape -- $out) (string escape -- $path)
-else if [ "$multiple" = 1 ]
-    set args --chooser-file=(string escape -- $out) (string escape -- $path)
+    set args --cwd-file (string escape -- $out)
 else
-    set args --chooser-file=(string escape -- $out) (string escape -- $path)
+    set args --chooser-file (string escape -- $out)
 end
 
-/usr/bin/footclient --title file_chooser -e /usr/bin/yazi $args
+footclient --title file_chooser -e yazi $args -- $path
 
 if [ "$save" = 1 ] && [ -s "$tmpfile" ]
     set selected_file $(/usr/bin/head -n 1 "$tmpfile")
