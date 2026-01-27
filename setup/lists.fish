9320cd5be79d65f5b1f0d73ab88139b3a7ec7866
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2026-01-27 15:48:31 +0530

    setup: android package list

---
+++ setup/lists.fish
@@ -0,0 +1,26 @@
+# script for updating setup/list/* files
+
+function pm
+    set -f ANDROID_USER_HOME "$XDG_DATA_HOME"/.android
+    set -f HOME "$XDG_DATA_HOME"
+
+    adb shell pm $argv
+end
+
+set pkglist "$HOME/repos/dotfiles/setup/pkglist"
+
+pacman -Qqe >"$pkglist/arch-pacman"
+
+ssh brick pacman -Qqe >"$pkglist/termux-pacman"
+
+pm list packages -i -3 | kt : 2 | kt 2 1 | sort >"$pkglist/android-3rd"
+
+pm list packages -d | kt : 2 >"$pkglist/android-disabled"
+
+### apply disabled apps list
+# set currently_disabled (mktemp -t disabled-XXXXX)
+# pm list packages -d | kt : 2 >"$currently_disabled"
+# for app in (cat ~/repos/dotfiles/setup/pkglist/android-disabled)
+#     rg -qF "$app" $currently_disabled
+#     or pm disable-user "$app" --user 0
+# end
