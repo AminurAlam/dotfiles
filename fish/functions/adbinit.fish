d54a8b08bb1fc46ff79889aab5b6e896d9f66881
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2026-01-29 08:39:53 +0530

    fish(adbinit): icons are managed by quickstar

--- fish/functions/adbinit.fish
+++ fish/functions/adbinit.fish
@@ -5,13 +5,13 @@ function adbinit -d "configure adb  settings with just one command"
     adb -d wait-for-device
 
     set -f shizuku_lib (adb -d shell pm path moe.shizuku.privileged.api | sed -E 's#^package:(.*)/base.apk$#\1/lib/arm64/libshizuku.so#')
-    set -f settings icon_blacklist bluetooth,nfc,power_saver,rotate,volte,ims_volte,ims_volte2,volume,wifi_calling,bluetooth_connected,mute,right_clock_position
+    # set -f settings icon_blacklist rotate,headset,fuseboxon,rotate,headset,ims_volte,ims_volte2,volume,mute,right_clock_position
 
     [ -n "$shizuku_lib" ] && adb -d shell $shizuku_lib
-    adb -d shell settings put secure $settings
+    # adb -d shell settings put secure $settings
 
     adb -d tcpip 5555
     adb -d connect (route -n | awk '/^[0.]+/{print $2}' | uniq | head -n1)
     [ -n "$shizuku_lib" ] && adb -e shell $shizuku_lib
-    adb -e shell settings put secure $settings
+    # adb -e shell settings put secure $settings
 end
