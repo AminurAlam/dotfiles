c1d749c3b8d42e445af5c95c2c09d0af3b484202
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2026-04-01 12:50:41 +0530

    nvim: use new commandline ui

--- nvim/init.lua
+++ nvim/init.lua
@@ -58,3 +58,27 @@ vim.diagnostic.config {
   update_in_insert = true,
   severity_sort = true,
 }
+
+require('vim._core.ui2').enable({
+      enable = true, -- Whether to enable or disable the UI.
+      msg = { -- Options related to the message module.
+        ---@type 'cmd'|'msg' Default message target, either in the
+        ---cmdline or in a separate ephemeral message window.
+        ---@type string|table<string, 'cmd'|'msg'|'pager'> Default message target
+        ---or table mapping |ui-messages| kinds and triggers to a target.
+        targets = {'cmd', 'msg', 'pager'},
+        cmd = { -- Options related to messages in the cmdline window.
+          height = 0.5 -- Maximum height while expanded for messages beyond 'cmdheight'.
+        },
+        dialog = { -- Options related to dialog window.
+          height = 0.5, -- Maximum height.
+        },
+        msg = { -- Options related to msg window.
+          height = 0.5, -- Maximum height.
+          timeout = 4000, -- Time a message is visible in the message window.
+        },
+        pager = { -- Options related to message window.
+          height = 1, -- Maximum height.
+        },
+      },
+    })
