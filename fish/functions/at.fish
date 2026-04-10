6a23c9ee122df279614adb9728e17cf341a7fd5a
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2026-04-10 09:41:54 +0530

    fish(at): for downloading torrents

---
+++ fish/functions/at.fish
@@ -0,0 +1,23 @@
+function at -d "torrent download helper"
+    if [ -z "$argv[1]" ]
+        pushd $XDG_DOWNLOAD_DIR/main/torrents/
+        set -f argv[1] $XDG_DOWNLOAD_DIR/main/torrents/(fd -d2 -tf --relative-path . | fzf)
+        popd
+    end
+    set -f files (aria2c $argv[1] -S | rg '\d+\|' | sed -E 's#\|.*/# #' | fzf --multi | kt 1 | string join ,)
+
+    set -f outdir (pwd)
+    if [ (path dirname $argv[1] | path basename) = anime ]
+        set -f outdir $XDG_VIDEOS_DIR
+    else if [ (path dirname $argv[1] | path basename) = manga ]
+        set -f outdir $XDG_DOWNLOAD_DIR/manga
+    else if string match -- '*/torrents/*' $outdir
+        set -f outdir $XDG_DOWNLOAD_DIR
+    end
+
+    printf "== %s ==\n" $argv[1] $outdir
+
+    [ -n "$files" ]
+    and aria2c --select-file $files --dir $outdir $argv[1]
+    or return 1
+end
