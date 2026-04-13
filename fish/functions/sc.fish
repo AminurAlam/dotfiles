db59e7b97306a79e6dee9e7074e374ec515a4533
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2026-04-13 18:46:18 +0530

    fish(sc): dont prompt for app cloning

--- fish/functions/sc.fish
+++ fish/functions/sc.fish
@@ -15,9 +15,10 @@ function sc -d "scrcpy wrapper"
             sudo modprobe v4l2loopback
             scrcpy -e --video-source camera --camera-id 0 --camera-ar sensor --v4l2-sink /dev/video2 -N & disown
         case "*"
-            set app (scrcpy -e --list-apps 2>/dev/null | rg --replace '' '^ (\*|-) ' | fzf -q "$argv[1]" | awk '{print $NF}')
-            [ -z "$app" ]
-            and scrcpy -eS -b 15M --max-fps 30 & disown
-            or scrcpy -eS -b 15M --max-fps 30 --new-display=960x1045/200 --no-vd-destroy-content --no-vd-system-decorations --start-app "$app" & disown
+            scrcpy -eS -b 15M --max-fps 30 & disown
+            # set app (scrcpy -e --list-apps 2>/dev/null | rg --replace '' '^ (\*|-) ' | fzf -q "$argv[1]" | awk '{print $NF}')
+            # [ -z "$app" ]
+            # and scrcpy -eS -b 15M --max-fps 30 & disown
+            # or scrcpy -eS -b 15M --max-fps 30 --new-display=960x1045/200 --no-vd-destroy-content --no-vd-system-decorations --start-app "$app" & disown
     end
 end
