d8ba88854aacdfb012b1012100d38189df0a56a5
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2026-05-30 17:47:28 +0530

    mpv: add default speed for yt videos

---
+++ mpv/scripts/yt-speed.lua
@@ -0,0 +1,9 @@
+local mp = require('mp')
+local utils = require('mp.utils')
+
+mp.register_event('start-file', function()
+  local dir, _ = utils.split_path(mp.get_property('path'))
+  if dir:match('^' .. os.getenv('XDG_VIDEOS_DIR') .. '/yt/$') then
+    mp.set_property('speed', 1.4)
+  end
+end)
