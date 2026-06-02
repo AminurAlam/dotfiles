95ab011aa9f1d03f458b7b38645474a67786f28a
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2026-06-02 20:31:10 +0530

    niri: cleanup rules and better single window looks

--- fish/functions/lg.fish
+++ fish/functions/lg.fish
@@ -1,13 +1,9 @@
 function lg -d "lazygit wrapper"
-    set -f old_size 0
-    set -f new_size 0
-    if [ -n "$NIRI_SOCKET" ]
-        set -f old_size (niri msg -j focused-window | jq .layout.window_size[0])
-        niri msg action set-column-width 100%
-        and set -f new_size (niri msg -j focused-window | jq .layout.window_size[0])
+    if [ -n "$NIRI_SOCKET" ] && [ (niri msg -j focused-window | jq .layout.window_size[0]) != 1920 ]
+        ctl toggle-width
+        LANG=en_US.UTF-8 lazygit
+        ctl toggle-width
+    else
+        LANG=en_US.UTF-8 lazygit
     end
-
-    LANG=en_US.UTF-8 lazygit
-
-    [ "$old_size" != "$new_size" ] && niri msg action set-column-width 50%
 end
