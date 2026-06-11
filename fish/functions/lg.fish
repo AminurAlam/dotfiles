63c63d43a941468e826f2114b347c3c8f1264c52
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2026-06-11 10:18:46 +0530

    fish(lg): handle egde case

--- fish/functions/lg.fish
+++ fish/functions/lg.fish
@@ -2,8 +2,10 @@ function lg -d "lazygit wrapper"
     if [ -n "$NIRI_SOCKET" ] && [ (niri msg -j focused-window | jq .layout.window_size[0]) != 1920 ]
         ctl toggle-width
         LANG=en_US.UTF-8 lazygit
-        ctl toggle-width
+        [ (niri msg -j focused-window | jq .layout.window_size[0]) = 1920 ]
+        and ctl toggle-width
     else
         LANG=en_US.UTF-8 lazygit
     end
+    :
 end
