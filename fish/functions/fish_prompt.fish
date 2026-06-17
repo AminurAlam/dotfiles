e459f3ac6145b3d7c29caebb5bf5049f9fdcf871
AminurAlam <64137875+AminurAlam@users.noreply.github.com>
2026-06-17 10:02:53 +0530

    fish(fish_prompt): push status

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt --description 'commandline prompt'
-    set -l last_status $pipestatus
+    set last_status $pipestatus
     set -l stat
     if [ (math (string join + $last_status)) -gt 0 ]
         set stat (set_color --bold red) ✘ (string join '|' $last_status)
@@ -18,15 +18,20 @@ function fish_prompt --description 'commandline prompt'
     set -l pwd (set_color $fish_color_cwd)  (prompt_pwd)
 
     set -l job
-    set -l jobcount (jobs | count)
+    set jobcount (jobs | count)
     if [ $jobcount -gt 0 ]
         set job (set_color blue)  $jobcount
     end
 
     set -l git
+    set -l push
     set branch (command git rev-parse --abbrev-ref HEAD 2>/dev/null)
     if [ -n "$branch" ]
         set git (set_color --bold purple)  $branch
+        set pushcount (git rev-list --count "origin/$branch..$branch")
+        if [ "$pushcount" -gt 0 ]
+            set push (set_color brcyan) ' ' $pushcount
+        end
     end
 
     set -l char '' '' \n' ❯'
@@ -48,6 +53,7 @@ function fish_prompt --description 'commandline prompt'
         $user \
         $pwd \
         $git \
+        $push \
         $job \
         $stat \
         $char
