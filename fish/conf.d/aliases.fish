f55954e4213343f853a14167c917e2e5bc5e286c
AminurAlam <oscorp2004@gmail.com>
2026-06-30 16:14:09 +0530

    fish(yt): use lockfile to queue downloads

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -23,6 +23,7 @@ abbr qmv "qmv -AXf do"
 abbr diff "diff -Naur"
 abbr py "python3 -q"
 abbr ru "rip url"
+abbr en LANG=en_US.UTF-8
 # abbr lg lazygit
 abbr pst "ps -faxo 'pid,comm' | sed -E \"s:\$PREFIX/[a-z]+/::\""
 abbr --set-cursor ff "ffmpeg -y -hide_banner -stats -loglevel error -i % -vcodec copy -acodec copy -map 0:a"
