ebe2e01c2a21bc65736edc73ba9fc711a3687b7b
AminurAlam <oscorp2004@gmail.com>
2026-07-17 21:45:40 +0530

    mpv: swap primary and secondary subs

--- mpv/scripts/init.lua
+++ mpv/scripts/init.lua
@@ -13,3 +13,40 @@ mp.register_event('start-file', function()
     mp.set_property('start', 0)
   end
 end)
+
+local sub_info = function(id)
+  if id == 'no' or id == nil then
+    return 'None'
+  end
+
+  local tracks = mp.get_property_native('track-list') or {}
+  for _, track in ipairs(tracks) do
+    if track.type == 'sub' and tostring(track.id) == tostring(id) then
+      return string.format('[%s] %s', (track.lang or '??'), (track.title or ''))
+    end
+  end
+
+  return 'Track ' .. id
+end
+
+local swap_subtitles = function()
+  local primary = mp.get_property('sid') or 'no'
+  local secondary = mp.get_property('secondary-sid') or 'no'
+
+  if primary == 'no' and secondary == 'no' then
+    mp.osd_message('No subtitles active to swap')
+    return
+  end
+
+  mp.set_property('sid', 'no')
+  mp.set_property('secondary-sid', 'no')
+  mp.set_property('sid', secondary)
+  mp.set_property('secondary-sid', primary)
+
+  mp.osd_message(string.format('%s\n%s', sub_info(primary), sub_info(secondary)))
+end
+
+mp.add_key_binding('a', 'swap-subtitles', swap_subtitles)
+
+-- TODO: bind `d` to automatically copy jp subs whether its in primary or secondary
+-- mp.add_key_binding('d', 'yank-jp-text', yank_jp_text)
