373bd07b1ba22e5eefa878f200b92df02441f1bd
AminurAlam <oscorp2004@gmail.com>
2026-08-06 21:37:58 +0530

    mpv: add better yank

--- mpv/scripts/init.lua
+++ mpv/scripts/init.lua
@@ -1,6 +1,8 @@
 local mp = require('mp')
 local utils = require('mp.utils')
 
+-- [[ event ]]
+
 mp.register_event('start-file', function()
   mp.set_property('resume-playback', 'no')
   local dir, file = utils.split_path(mp.get_property('path'))
@@ -13,6 +15,8 @@ mp.register_event('start-file', function()
     mp.set_property('start', 0)
   end
 end)
+
+-- [[ swap subs ]]
 
 local sub_info = function(id)
   if id == 'no' or id == nil then
@@ -46,7 +50,47 @@ local swap_subtitles = function()
   mp.osd_message(string.format('%s\n%s', sub_info(primary), sub_info(secondary)))
 end
 
-mp.add_key_binding('a', 'swap-subtitles', swap_subtitles)
+-- [[ yank subs ]]
 
--- TODO: bind `d` to automatically copy jp subs whether its in primary or secondary
--- mp.add_key_binding('d', 'yank-jp-text', yank_jp_text)
+---@return "sub-text"|"secondary-sub-text"|nil
+local get_jp_loc = function()
+  local primary_sid, secondary_sid = mp.get_property('sid'), mp.get_property('secondary-sid')
+  local tracks = mp.get_property_native('track-list') or {}
+
+  for _, track in ipairs(tracks) do
+    if
+      track.type == 'sub'
+      and track.selected == true
+      and (track.lang == 'ja' or track.lang == 'jp')
+    then
+      if tostring(track.id) == tostring(primary_sid) then
+        return 'sub-text'
+      elseif tostring(track.id) == tostring(secondary_sid) then
+        return 'secondary-sub-text'
+      end
+    end
+    -- for k, v in pairs(track) do
+    --   print(k, v)
+    -- end
+  end
+end
+
+local yank_jp_text = function()
+  local ps = get_jp_loc() ---@type "sub-text"|"secondary-sub-text"|nil
+  if not ps then
+    return
+  end
+
+  local text = mp.get_property(ps) ---@type string?
+  if not text or text == '' then
+    return
+  end
+
+  -- TODO: strip spaces from text
+  text = string.gsub(text, ' ', '')
+  mp.commandv('run', 'wl-copy', text)
+  print(text)
+end
+
+mp.add_key_binding('a', 'swap-subtitles', swap_subtitles)
+mp.add_key_binding('d', 'yank-jp-text', yank_jp_text)
