5e1dc39f8cefe9653d008c8cfd3811a32402e098
AminurAlam <oscorp2004@gmail.com>
2026-08-13 01:10:19 +0530

    fish(at): add magnet support

--- fish/functions/at.fish
+++ fish/functions/at.fish
@@ -1,13 +1,18 @@
 function at -d "torrent download helper"
-    # TODO: auto create tmux session
     if [ -z "$argv[1]" ]
         pushd $XDG_DOWNLOAD_DIR/main/torrents/
         set -f argv[1] $XDG_DOWNLOAD_DIR/main/torrents/(fd -d2 -tf --relative-path . | fzf)
         popd
+    else if string match -q -- 'magnet:*' "$argv[1]"
+        aria2c --bt-save-metadata --bt-metadata-only "$argv[1]"
+        set file (echo "$argv[1]" | rg --only-matching '[0-9a-f]{40}').torrent
+        [ -e "$file" ]
+        and set argv[1] "$file"
+        or return
     end
     [ -z "$argv[1]" ] && return 1
 
-    set -f files (aria2c $argv[1] -S | rg '\d+\|' | sed -E 's#\|.*/# #' | fzf --multi | kt 1 | string join ,)
+    set -f files (aria2c $argv[1] -S | rg '\d+\|' | sed -E 's#\|.*/# #' | fzf --preview 'aria2c -S {}' --multi | kt 1 | string join ,)
 
     # TODO: termux specif outdir
     set -f outdir (pwd)
