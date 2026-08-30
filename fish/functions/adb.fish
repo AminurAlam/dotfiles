0624252262b449b6262302c6eebc4aa55966ef49
AminurAlam <oscorp2004@gmail.com>
2026-08-30 10:32:57 +0530

    fish(adb): simplify tcp stuff

--- fish/functions/adb.fish
+++ fish/functions/adb.fish
@@ -3,30 +3,25 @@ function adb -d "adb wrapper"
     set -f HOME "$XDG_DATA_HOME"
 
     if [ "$argv[1]" = init ]
-        if not command adb devices | rg -q 5555
+        set ip (route -n | awk '/^[0.]+/{print $2}' | uniq | head -n1)
+
+        if [ -n "$ip" ]
             command adb -d tcpip 5555
-            [ -n "$ip" ]
-            and command adb connect "$ip"
-            or command adb connect (route -n | awk '/^[0.]+/{print $2}' | uniq | head -n1)
+
+            command adb connect "$ip"
+            command adb connect android.local
+            and command adb disconnect "$ip"
         end
 
-        if [ (command adb devices | count) -ge 3 ] \
-                && not command adb devices | rg -q android.local
-
-            if command adb connect android.local
-                set old_ip (command adb devices | rg 5555 | rg -v android.local)
-                [ -n "$old_ip" ]
-                and command adb disconnect
-            end
-        end
+        command adb devices
 
         for n in 1 3 4 6
-            adb reverse tcp:1000$n tcp:1000$n
+            # adb reverse tcp:1000$n tcp:1000$n
         end
 
-        set -f shizuku_lib (command adb -e shell pm path moe.shizuku.privileged.api | sed -E 's#^package:(.*)/base.apk$#\1/lib/arm64/libshizuku.so#')
-        [ -n "$shizuku_lib" ] && command adb -e shell $shizuku_lib
-        command adb -e shell settings put secure icon_blacklist rotate,headset,fuseboxon,rotate,headset,ims_volte,ims_volte2,volume,mute
+        # set -f shizuku_lib (command adb -e shell pm path moe.shizuku.privileged.api | sed -E 's#^package:(.*)/base.apk$#\1/lib/arm64/libshizuku.so#')
+        # [ -n "$shizuku_lib" ] && command adb -e shell $shizuku_lib
+        # command adb -e shell settings put secure icon_blacklist rotate,headset,fuseboxon,rotate,headset,ims_volte,ims_volte2,volume,mute
         command adb -e shell settings put secure multi_audio_focus_enabled 0
         command adb -e shell settings put secure sysui_quick_bar_collapsed_row 3
         command adb -e shell settings put system adjust_media_volume_only 1
